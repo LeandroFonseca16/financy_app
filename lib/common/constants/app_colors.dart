da1ac79b2eb6c182a1ffa39a41f9a20a4e766790
Leandro <leandro.fonseca1621@gmail.com>
2024-11-22 10:52:17 -0300

    feat

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -4,6 +4,5 @@ class AppColors {
   AppColors._();
 
   static const Color greelightOne = Color (0xFF63B5AF);
-  static const Color greelightTwo = Color (0xFF438883);
-  static const List<color>
+  static const Color greelightTwo = Color (0xFF438813);
 }
